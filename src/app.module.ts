e2a5d0417b21731ad6a50d37e9c8923f82e928a7
Bharat Shah <akashche72@gmail.com>
2023-12-10 13:51:30 +0400

    adding car and user entity to application skaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CarsModule } from './cars/cars.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Car } from './cars/car.entity';
 
 @Module({
-  imports: [UsersModule, CarsModule],
+  // imports: [UsersModule, CarsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService], 
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite', 
+          database: config.get<string>('DB_NAME'), 
+          entities: [User, Car]
+        }
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

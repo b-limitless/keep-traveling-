589803ad8f67181295f41d18798bd8e56c1ac827
Bharat Shah <akashche72@gmail.com>
2023-12-10 20:02:59 +0400

    adding reservation service to reservation module

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReservationsController } from './reservations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reservation } from './reservation.entity';
+import { ReservationService } from './reservations.service';
 
 @Module({
-  controllers: [ReservationsController]
+  imports: [TypeOrmModule.forFeature([Reservation])],
+  controllers: [ReservationsController], 
+  providers: [ReservationService]
 })
 export class ReservationsModule {}

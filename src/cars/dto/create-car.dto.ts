220e415f1c5571124923125a310e25d84a096675
Bharat Shah <akashche72@gmail.com>
2023-12-10 18:14:00 +0400

    adding create car dot

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,3 +1,29 @@
-export class CreateCarDto {
+import {
+    IsNumber,
+    IsString,
+    Max,
+    Min
+} from 'class-validator';
+  
+  export class CreateCarDto {
+    @Min(1)
+    @Max(1000000)
+    price: number;
     
-}
+    @IsString()
+    make: string;
+    
+    @IsString()
+    model: string;
+    
+    @IsNumber()
+    @Min(1920)
+    @Max(2050)
+    year: number;
+    
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
+    mileage: number;
+  }
+  

eb12388b590ce7ac9c4e3baa425179902c91aecd
Bharat Shah <akashche72@gmail.com>
2023-12-10 14:32:39 +0400

    adding user entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,3 +1,24 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
-    
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  admin: boolean;
 }

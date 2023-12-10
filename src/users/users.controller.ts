9112c4ff484c5e65447febbb68e79111b4333c1c
Bharat Shah <akashche72@gmail.com>
2023-12-10 15:56:51 +0400

    adding some http route to handle signup, signin and signout

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,30 @@
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { Post, Body, Session } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(
-            private authService: AuthService, 
-            private userService: UsersService
-            ) {
+  constructor(
+    private authService: AuthService,
+    private userService: UsersService,
+  ) {}
 
-    }
+  @Post('/signup')
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
 }

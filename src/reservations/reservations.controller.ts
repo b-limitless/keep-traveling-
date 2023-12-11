edf59fd166e919a22d82ad3c4997cd14aff3d4ce
Bharat Shah <akashche72@gmail.com>
2023-12-11 15:42:52 +0400

    creating reservatinon service in controller - creating reservation

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { Post, Body } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
+import { ReservationService } from './reservations.service';
 
 @Controller('reservations')
-export class ReservationsController {}
+export class ReservationsController {
+    constructor(private reservationService: ReservationService) {}
+    @Post('/')
+    createReservation(@CurrentUser() user: User, @Body() body: CreateReservationDto) {
+        return this.reservationService.create(body, user);
+    }
+
+}
